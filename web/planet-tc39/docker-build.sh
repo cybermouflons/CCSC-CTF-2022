2433791e71157fcebe41bcaa60e58cc1273f4e77
Kyriakos Georgiou <kyriakos.a.georgiou@gmail.com>
2022-04-03 11:39:52 -0400

    fix(web/planet-tc39): public tarball creation

--- web/planet-tc39/docker-build.sh
+++ web/planet-tc39/docker-build.sh
@@ -4,5 +4,5 @@ docker build ./setup -t ${IMAGE_NAME}
 
 CID=$(docker create ${IMAGE_NAME})
 
-docker cp ${CID}:/ctf/challenge/planet-tc39.tar.gz ./public/
+docker cp ${CID}:/ctf/challenge/static/planet-tc39.tar.gz ./public/
 docker rm ${CID}
