4020b25912e5cebe6a20aafeeb241f68f13de5da
Kyriakos Georgiou <kyriakos.a.georgiou@gmail.com>
2022-04-30 15:29:00 -0400

    stego(ufo): update

--- stego/ufo/setup/build.py
+++ stego/ufo/setup/build.py
@@ -7,10 +7,10 @@ OUTPUT_AUDIO_FILE_NAME = 'ufo.mp3'
 
 sound = AudioSegment.from_mp3(f'./{INPUT_AUDIO_FILE_NAME}')
 
-sound_clip_0 = sound[35596:35596 + 225]
-sound_clip_1 = sound[35288:35288 + 225]
+sound_clip_0 = sound[35288:35288 + 225]
+sound_clip_1 = sound[35596:35596 + 225]
 
-flag_bin = ''.join([bin(x)[2:] for x in FLAG.encode('ascii')])
+flag_bin = ''.join([bin(x)[2:].zfill(8) for x in FLAG.encode('ascii')])
 
 soundmap = {'0': sound_clip_0, '1': sound_clip_1}
 
